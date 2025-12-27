1e5ef7cd3818d7299bc7f78e4e503717924ae5f3
Rin-repo-sun <48145001+Rin-repo-sun@users.noreply.github.com>
2025-12-27 22:52:53 +0900

    Initial commit: Add daily output habit tracker app

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+import { HabitTracker } from './components/habit-tracker';
+
+export default function App() {
+  return (
+    <div className="size-full">
+      <HabitTracker />
+    </div>
+  );
+}
